92df096d079ce9c9d0b580d6ca070c995546b8f4
Mark Piper <mark.piper@colorado.edu>
2015-11-06 16:59:05 -0700

    Begin writing unit tests for Heat class

---
+++ testing/edu/colorado/csdms/heat/HeatTest.java
@@ -0,0 +1,203 @@
+package edu.colorado.csdms.heat;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test for the Heat class.
+ */
+public class HeatTest {
+
+  private List<Integer> shape;
+  private Double alpha;
+  private Double time;
+  private Double timeStep;
+  private Heat testHeat;
+  
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    Integer nRows = 10;
+    Integer nCols = 20;
+    Double dx = 1.0;
+    Double dy = 1.0;
+    Double xStart = 0.0;
+    Double yStart = 0.0;
+    alpha = 1.0;
+    testHeat = new Heat(nRows, nCols, dx, dy, xStart, yStart, alpha);
+    time = 0.0;
+    timeStep = 0.25;
+    shape = new ArrayList<Integer>(Arrays.asList(nRows, nCols));
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#Heat(java.lang.Integer, java.lang.Integer, java.lang.Double, java.lang.Double, java.lang.Double, java.lang.Double, java.lang.Double)}.
+   */
+  @Test
+  public final void testHeatIntegerIntegerDoubleDoubleDoubleDoubleDouble() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#Heat()}.
+   */
+  @Test
+  public final void testHeat() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#Heat(java.lang.String)}.
+   */
+  @Test
+  public final void testHeatString() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getShape()}.
+   */
+  @Test
+  public final void testGetShape() {
+    assertEquals(shape, testHeat.getShape());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setShape(java.util.List)}.
+   */
+  @Test
+  public final void testSetShape() {
+    List<Integer> newShape = new ArrayList<Integer>();
+    newShape.add(100);
+    newShape.add(50);
+    testHeat.setShape(newShape);
+    assertEquals(newShape, testHeat.getShape());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getSpacing()}.
+   */
+  @Test
+  public final void testGetSpacing() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setSpacing(java.util.List)}.
+   */
+  @Test
+  public final void testSetSpacing() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getOrigin()}.
+   */
+  @Test
+  public final void testGetOrigin() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setOrigin(java.util.List)}.
+   */
+  @Test
+  public final void testSetOrigin() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getAlpha()}.
+   */
+  @Test
+  public final void testGetAlpha() {
+    assertEquals(alpha, testHeat.getAlpha());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setAlpha(java.lang.Double)}.
+   */
+  @Test
+  public final void testSetAlpha() {
+    Double newAlpha = 0.2;
+    testHeat.setAlpha(newAlpha);
+    assertEquals(newAlpha, testHeat.getAlpha());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getTime()}.
+   */
+  @Test
+  public final void testGetTime() {
+    assertEquals(time, testHeat.getTime());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setTime(java.lang.Double)}.
+   */
+  @Test
+  public final void testSetTime() {
+    Double newTime = 42.0;
+    testHeat.setTime(newTime);
+    assertEquals(newTime, testHeat.getTime());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getTimeStep()}.
+   */
+  @Test
+  public final void testGetTimeStep() {
+    assertTrue(testHeat.getTimeStep() > 0.0);
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setTimeStep(java.lang.Double)}.
+   */
+  @Test
+  public final void testSetTimeStep() {
+    Double newTimeStep = 5.0;
+    testHeat.setTimeStep(newTimeStep);
+    assertEquals(newTimeStep, testHeat.getTimeStep());
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#getTemperature()}.
+   */
+  @Test
+  public final void testGetTemperature() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#setTemperature(double[][])}.
+   */
+  @Test
+  public final void testSetTemperature() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link edu.colorado.csdms.heat.Heat#advanceInTime()}.
+   */
+  @Test
+  public final void testAdvanceInTime() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
